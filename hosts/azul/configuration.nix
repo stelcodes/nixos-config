b5ea880e6af90ac7398bb341853354949ab60dfd
Stel Abrego <stel@stel.codes>
2021-05-16 16:50:19 -0400

    remove jack config

--- hosts/azul/configuration.nix
+++ hosts/azul/configuration.nix
@@ -146,29 +146,6 @@
 
     # don’t shutdown when power button is short-pressed
     logind.extraConfig = "HandlePowerKey=ignore";
-
-    # jack = {
-    #   jackd = {
-    #     enable = true;
-    #     # from Arch Wiki https://is.gd/RXY6lR
-    #     # session = ''
-    #     #   jack_control start
-    #     #   jack_control ds alsa
-    #     #   jack_control dps device hw:HDA
-    #     #   jack_control dps rate 48000
-    #     #   jack_control dps nperiods 2
-    #     #   jack_control dps period 64
-    #     #   sleep 10
-    #     #   a2j_control --ehw
-    #     #   a2j_control --start
-    #     #   sleep 10
-    #     #   qjackctl &
-    #     # '';
-    #     session = "";
-    #   };
-    #   alsa.enable = false;
-    #   loopback.enable = true;
-    # };
   };
 
   users = {
