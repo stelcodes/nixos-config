e77f2e1cb2d1d85539fe32b55fc2634923fb04d7
Stel Abrego <stel@stel.codes>
2021-05-25 15:23:35 -0400

    add common server config

---
+++ modules/server.nix
@@ -0,0 +1,14 @@
+{ pkgs, config, ... }: {
+  config = {
+
+    networking.firewall.allowPing = true;
+
+    services.openssh.enable = true;
+    services.openssh.passwordAuthentication = false;
+
+    users.users.stel.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFl1QCu19AUDFaaZZAt4YtnxxdX+JDvDz5rdnBEfH/Bb stel@azul"
+    ];
+
+  };
+}
