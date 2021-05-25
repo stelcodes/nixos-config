eb2177471ea948e366c402e7e589fd43f13e7bc3
Stel Abrego <stel@stel.codes>
2021-05-25 15:24:52 -0400

    switch morado1 to the new common server config

--- hosts/morado1/configuration.nix
+++ hosts/morado1/configuration.nix
@@ -3,21 +3,12 @@
     ./hardware-configuration.nix
     ./networking.nix # generated at runtime by nixos-infect
     /home/stel/config/modules/common.nix
+    /home/stel/config/modules/server.nix
     <home-manager/nixos>
   ];
 
-  boot.cleanTmpDir = true;
-
   networking.hostName = "morado1";
-  networking.firewall.allowPing = true;
   networking.firewall.allowedTCPPorts = [ 22 ];
-
-  services.openssh.enable = true;
-  services.openssh.passwordAuthentication = false;
-
-  users.users.stel.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFl1QCu19AUDFaaZZAt4YtnxxdX+JDvDz5rdnBEfH/Bb stel@azul"
-  ];
 
   home-manager = {
     useGlobalPkgs = true;
