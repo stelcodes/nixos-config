33b40b2ff670fe9ad4aaa43c9f88c7f1189b2164
Stel Abrego <stel@stel.codes>
2021-05-25 15:13:03 -0400

    Merge morado1 config with the rest of the config

--- hosts/morado1/configuration.nix
+++ hosts/morado1/configuration.nix
@@ -1,18 +1,24 @@
-{ pkgs, config, ... }: {
+{ pkgs, ... }: {
   imports = [
     ./hardware-configuration.nix
     ./networking.nix # generated at runtime by nixos-infect
-    ./host.nix
+    /home/stel/config/modules/common.nix
     <home-manager/nixos>
   ];
 
   boot.cleanTmpDir = true;
+
   networking.hostName = "morado1";
   networking.firewall.allowPing = true;
+  networking.firewall.allowedTCPPorts = [ 22 ];
+
   services.openssh.enable = true;
-  users.users.root.openssh.authorizedKeys.keys = [
+  services.openssh.passwordAuthentication = false;
+
+  users.users.stel.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFl1QCu19AUDFaaZZAt4YtnxxdX+JDvDz5rdnBEfH/Bb stel@azul"
   ];
+
   home-manager = {
     useGlobalPkgs = true;
     users.stel = { config, ... }:
