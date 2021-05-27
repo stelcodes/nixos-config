a2893f9077c2ef596b3963d79e00ac1d2c5d991d
Stel Abrego <stel@stel.codes>
2021-05-27 16:37:01 -0400

    add zprint config

--- home-manager/clojure/default.nix
+++ home-manager/clojure/default.nix
@@ -4,6 +4,9 @@ pkgs: {
     # I'm having a weird bug where clj -X:new gives an error about :exec-fn not being set even though it's set...
     # So I'm trying to put the deps.edn in the .config directory as well as the .clojure directory
     # I don't think this helped I had to use clj -X:new:clj-new/create
-    file = { ".clojure/deps.edn".source = /home/stel/config/misc/deps.edn; };
+    file = {
+      ".clojure/deps.edn".source = /home/stel/config/misc/deps.edn;
+      ".zprintrc".text = "{:width 120}";
+    };
   };
 }
