3a6954b9f0488cd7cddeb9e172650d90eb9316dd
Stel Abrego <stel@stel.codes>
2021-05-28 11:19:29 -0400

    update gitea config

--- hosts/morado1/configuration.nix
+++ hosts/morado1/configuration.nix
@@ -39,14 +39,29 @@
     rootUrl = "https://git.stel.codes";
     httpPort = 3000;
     cookieSecure = true;
+    disableRegistration = true;
     settings = {
+      api = { ENABLE_SWAGGER = false; };
       repository = {
         DISABLE_HTTP_GIT = true;
         DEFAULT_BRANCH = "main";
       };
       ui = { DEFAULT_THEME = "arc-green"; };
+      security = {
+        INSTALL_LOCK = true;
+        SECRET_KEY =
+          "IvDiP5wNKoNkYg7rlmHfQZVxAOCl6xYPorYXV4t746GQ7GNlMYiarqcKCsqUXFNT";
+      };
+      mail = {
+        ENABLES = true;
+        FROM = "gitea@git.stel.codes";
+        MAILER_TYPE = "smtp";
+        HOST = "smtp.mailgun.org:587";
+        IS_TLS_ENABLED = true;
+        USER = "postmaster@sandbox6b576e2e5db145fa8a2833aebe918517.mailgun.org";
+        PASSWORD = "0450caa70c93fe11c7c0b782915954ca-fa6e84b7-0e581df9";
+      };
     };
-    disableRegistration = true;
   };
 
   home-manager = {
