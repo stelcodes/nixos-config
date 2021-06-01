86ad93f72f7c89a71bdee08260f73c3a6a6a7d4f
Stel Abrego <stel@stel.codes>
2021-06-01 19:16:19 -0400

    formatting

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,25 +1,23 @@
 pkgs:
-pkgs.lib.mkMerge [
-  {
-    xdg = {
-      userDirs = {
-        enable = true;
-        desktop = "$HOME/desktop";
-        documents = "$HOME/documents";
-        download = "$HOME/downloads";
-        music = "$HOME/music";
-        pictures = "$HOME/pictures";
-        publicShare = "$HOME/public";
-        templates = "$HOME/template";
-        videos = "$HOME/videos";
-      };
+pkgs.lib.mkMerge [{
+  xdg = {
+    userDirs = {
+      enable = true;
+      desktop = "$HOME/desktop";
+      documents = "$HOME/documents";
+      download = "$HOME/downloads";
+      music = "$HOME/music";
+      pictures = "$HOME/pictures";
+      publicShare = "$HOME/public";
+      templates = "$HOME/template";
+      videos = "$HOME/videos";
     };
-    home = {
-      username = "stel";
-      stateVersion = "21.03";
-      # I'm putting all manually installed executables into ~/.local/bin 
-      sessionPath = [ "$HOME/.local/bin" ];
-    };
-    programs.home-manager.enable = true;
-  }
-]
+  };
+  home = {
+    username = "stel";
+    stateVersion = "21.03";
+    # I'm putting all manually installed executables into ~/.local/bin 
+    sessionPath = [ "$HOME/.local/bin" ];
+  };
+  programs.home-manager.enable = true;
+}]
