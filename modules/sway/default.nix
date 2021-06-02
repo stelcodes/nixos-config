a60d5edd0c7aac95e8a054fd0e8a80dd3af92f97
Stel Abrego <stel@stel.codes>
2021-06-02 19:37:32 -0400

    ripping out home-manager - sway

---
+++ modules/sway/default.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }: {
+  config = {
+    programs.sway.enable = true;
+    programs.sway.extraPackages = with pkgs; [
+      swaylock
+      swayidle
+      dmenu
+      brightnessctl
+      playerctl
+      libinput
+      xorg.xev
+      gnome3.nautilus
+      keepassxc
+      font-manager
+      gnome3.seahorse
+      wl-clipboard
+      wofi
+      gnome3.adwaita-icon-theme # for the two icons in the default wofi setup
+      wlsunset
+      # sway screenshots
+      grim
+      slurp
+    ];
+    # mkdir $HOME/.config/sway && ln -s /etc/sway-config $HOME/.config/sway/config
+    environment.etc."sway-config".source = ./sway-config;
+    # mkdir $HOME/.config/wofi && ln -s /etc/wofi-style.css $HOME/.config/wofi/style.css
+    environment.etc."wofi-style.css".source = ./wofi-style.css;
+    # Waybar checks /etc/xdg/waybar for configuration files so no need to create links
+    environment.etc."xdg/waybar/style.css".source = ./waybar-style.css;
+    environment.etc."xdg/waybar/config".source = ./waybar-config.json;
+  };
+}
