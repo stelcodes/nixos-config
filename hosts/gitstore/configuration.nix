c0b5551278fad696ac6b7f1e562fa676aa5fdc1e
Stel Abrego <stel@stel.codes>
2021-06-03 13:53:10 -0400

    allow gitstore git user to ssh into itself

--- hosts/gitstore/configuration.nix
+++ hosts/gitstore/configuration.nix
@@ -13,6 +13,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFl1QCu19AUDFaaZZAt4YtnxxdX+JDvDz5rdnBEfH/Bb stel@azul"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBj6nr06BHdwsxcbSgMyPy5e6UghJgY7R9mTdmg4d9hx stel@nube1"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJJJxJN5jyGvdGsGxwxWWw33ecF4lO0j7txQZRiQMTzs stel@gitstore"
     ];
     isNormalUser = true;
   };
