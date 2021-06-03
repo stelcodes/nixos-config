9c3f4bc3018af74ee9893ac630aba1eaba034140
Stel Abrego <stel@stel.codes>
2021-06-03 18:33:17 -0400

    Update tmux conf location

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -28,7 +28,7 @@
       bind -n M-c copy-mode
       bind -n M-r command-prompt 'rename-session %%'
       bind -n M-n command-prompt 'new-session'
-      bind -n M-t source-file ~/.config/tmux/tmux.conf \; display-message "Config reloaded!"
+      bind -n M-t source-file /etc/tmux.conf \; display-message "Config reloaded!"
 
       # Fixes tmux escape input lag, see https://git.io/JtIsn
       set -sg escape-time 10
