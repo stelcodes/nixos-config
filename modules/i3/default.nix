4fc975bb3eb047ccd329b8b8c7e553a20084abb0
Stel Abrego <stel@stel.codes>
2021-07-25 11:14:48 -0600

    add locking, screen captures to i3, clean up config

--- modules/i3/default.nix
+++ modules/i3/default.nix
@@ -1,26 +1,16 @@
 { pkgs, ... }: {
   services.xserver.enable = true;
-  services.xserver.autorun = true;
   services.xserver.libinput.enable = true;
   services.xserver.libinput.touchpad.naturalScrolling = true;
   services.xserver.layout = "us";
   services.xserver.xkbOptions = "caps:escape";
   services.xserver.xkbVariant = "mac";
   services.xserver.desktopManager.xterm.enable = false;
-  # services.xserver.displayManager.session = [{
-  #   manage = "window";
-  #   name = "sway";
-  #   start = "exec sway";
-  # }];
-  services.xserver.displayManager.session = [];
-  services.xserver.displayManager.autoLogin.enable = false;
-  services.xserver.displayManager.autoLogin.user = "stel";
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.displayManager.lightdm.background = /home/stel/pictures/wallpapers/pretty-nord.jpg;
   services.xserver.displayManager.defaultSession = "none+i3";
   services.xserver.windowManager.i3.enable = true;
   services.xserver.windowManager.i3.configFile = ./i3-config;
-  programs.zsh.shellAliases."xgui" = "doas systemctl start graphical.target";
-  environment.systemPackages = [ pkgs.feh ];
+  environment.systemPackages = [ pkgs.feh pkgs.xidlehook pkgs.xss-lock pkgs.escrotum ];
   environment.etc."i3status.conf".source = ./i3status.conf;
 }
