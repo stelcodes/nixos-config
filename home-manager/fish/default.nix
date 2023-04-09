3c7113cdc92b210ebe498f50e06984e252010369
Stel Abrego <stel@stel.codes>
2023-04-09 10:03:04 -0700

    Add more fish abbreviations

--- home-manager/fish/default.nix
+++ home-manager/fish/default.nix
@@ -3,7 +3,8 @@ pkgs: {
   programs.fish = {
     enable = true;
     interactiveShellInit = builtins.readFile ./interactive.fish;
-    shellAbbrs = {
+    shellAbbrs = let
+    in rec {
       ll = "ls -l";
       la = "ls -A";
       rm = "rm -i";
@@ -20,8 +21,20 @@ pkgs: {
       config = "cd ~/nixos-config && nvim";
       d = "dua --stay-on-filesystem interactive";
       new-ssh-key = "ssh-keygen -t ed25519 -C 'stel@stel.codes'";
-      # ISO 8601 date format with UTC timezone
-      date-iso = "date -u +\"%Y-%m-%dT%H:%M:%SZ\"";
+      date-iso = "date -u +\"%Y-%m-%dT%H:%M:%SZ\""; # ISO 8601 date format with UTC timezone
+      t = "tmux attach -t config; or tmux";
+      n = "nnn -eauUA";
+      beep = "timeout -s KILL 0.15 speaker-test --frequency 400 --test sin";
+      dl-base = "yt-dlp --embed-metadata --embed-thumbnail --progress";
+      dl-video = "${dl-base} --embed-subs --sub-langs 'en' --embed-chapters --sponsorblock-mark 'default' --sponsorblock-remove 'sponsor,selfpromo,outro' --remux-video 'mkv'";
+      dl-video-best = "${dl-video} --format best";
+      dl-video-1080 = "${dl-video} --format 'worstvideo[height=1080]+bestaudio / best[height<=1080]'";
+      dl-video-1080-playlist = "${dl-video-1080} --output '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'";
+      dl-music =  "${dl-base} --format 'bestaudio[ext=ogg] / bestaudio[ext=mp3]' --output '%(album_artist,artist|unknown artist)s/%(album,track|unknown album)s/%(track_number|0)s %(track,id,epoch)s.%(ext)s'";
+      dl-music-best =  "${dl-music} --format 'bestaudio[ext=flac] / bestaudio[ext=ogg] / bestaudio[ext=mp3]'";
+      dl-music-yt = "${dl-base} --format 'bestaudio' --extract-audio --audio-format opus";
+      noansi = "sed \"s,\\x1B\\[[0-9;]*[a-zA-Z],,g\"";
+      loggy = "${noansi} | tee ~/tmp/$(date +%F-%T)-log.txt";
     };
   };
 }
--- home-manager/fish/interactive.fish
+++ home-manager/fish/interactive.fish
@@ -1,26 +1,15 @@
-fish_vi_key_bindings
-fish_add_path /nix/var/nix/profiles/default/bin ~/.nix-profile/bin ~/.cargo/bin ~/go/bin ~/.local/bin
-
-# ENVIRONMENT #################################################################
-# Important environment variables
 set -x BROWSER "firefox"
 set -x EDITOR "nvim"
 set -x PAGER "less --chop-long-lines --RAW-CONTROL-CHARS"
 set -x MANPAGER 'nvim +Man!'
-# This sets the theme for bat and delta
-set -x BAT_THEME 'base16'
+set -x BAT_THEME 'base16' # For bat and delta
 set -x NNN_TRASH 1
 set -x NNN_PLUG 'p:preview-tui;d:dragdrop'
 set -x NNN_TMPFILE '/tmp/nnn-last-dir'
 set -x NNN_FCOLORS "030304030705020801030301"
 
-# FUNCTIONS ###################################################################
-function n
-  nnn -eauUA
-  # cd into directory when quitting
-  cd $(cat /tmp/nnn-last-dir | string sub --start 5 --end -1)
-end
-
 set -g fish_greeting (printf (_ '🐟 don\'t be afraid to ask for %shelp%s 💞') (set_color green) (set_color normal))
+fish_vi_key_bindings
+fish_add_path /nix/var/nix/profiles/default/bin ~/.nix-profile/bin ~/.cargo/bin ~/go/bin ~/.local/bin
 starship init fish | source
 direnv hook fish | source
