6e0ecee0c1be7203409accf96f1cf1ca56b44a2b
Stel Abrego <stel@stel.codes>
2023-04-09 11:43:53 -0700

    Clean up hm default packages

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -20,38 +20,18 @@ pkgs: {
   home = {
 
     packages = [
-      # process monitoring
       pkgs.htop
-      pkgs.procs
-      # cross platform trash bin
       pkgs.trash-cli
-      # alternative find, also used for fzf
       pkgs.fd
-      # system info
       pkgs.neofetch
-      # http client
       pkgs.httpie
-      # download stuff from the web
       pkgs.wget
-      # searching text
       pkgs.ripgrep
-      # documentaion
       pkgs.tealdeer
-      # archiving
       pkgs.unzip
-      # backups
       pkgs.restic
-      # ls replacement
-      pkgs.exa
-      # make replacement
-      pkgs.just
-      # math
-      pkgs.rink
-      # nix
-      pkgs.nixfmt
       pkgs.nix-prefetch-github
-      # timeless db
-      pkgs.sqlite
+      pkgs.babashka
     ];
 
     # I'm putting all manually installed executables into ~/.local/bin
