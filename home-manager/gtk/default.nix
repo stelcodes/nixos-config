59e924b049d58f44964d31a142d5b740d74ab50b
Stel Abrego <stel@stel.codes>
2023-04-09 10:03:26 -0700

    Add gtk icons

--- home-manager/gtk/default.nix
+++ home-manager/gtk/default.nix
@@ -9,5 +9,18 @@ pkgs: {
       package = pkgs.nordic;
       name = "Nordic";
     };
+    iconTheme = {
+      package = pkgs.nordzy-icon-theme;
+      name = "Nordzy";
+    };
+    # Cursors only display on GTK apps, kind of annoying
+    # cursorTheme = {
+    #   package = pkgs.nordzy-cursor-theme;
+    #   name = "Nordzy-cursors";
+    # };
+  };
+  qt = {
+    enable = true;
+    platformTheme = "gtk";
   };
 }
