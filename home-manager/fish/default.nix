69a53d4bdaf07dc51fff56254a018167aa516018
Stel Abrego <stel@stel.codes>
2023-04-11 20:58:57 -0700

    Format fish config

--- home-manager/fish/default.nix
+++ home-manager/fish/default.nix
@@ -3,8 +3,7 @@ pkgs: {
   programs.fish = {
     enable = true;
     interactiveShellInit = builtins.readFile ./interactive.fish;
-    shellAbbrs = let
-    in rec {
+    shellAbbrs = rec {
       ll = "ls -l";
       la = "ls -A";
       rm = "rm -i";
@@ -30,8 +29,8 @@ pkgs: {
       dl-video-best = "${dl-video} --format best";
       dl-video-1080 = "${dl-video} --format 'worstvideo[height=1080]+bestaudio / best[height<=1080]'";
       dl-video-1080-playlist = "${dl-video-1080} --output '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'";
-      dl-music =  "${dl-base} --format 'bestaudio[ext=ogg] / bestaudio[ext=mp3]' --output '%(album_artist,artist|unknown artist)s/%(album,track|unknown album)s/%(track_number|0)s %(track,id,epoch)s.%(ext)s'";
-      dl-music-best =  "${dl-music} --format 'bestaudio[ext=flac] / bestaudio[ext=ogg] / bestaudio[ext=mp3]'";
+      dl-music = "${dl-base} --format 'bestaudio[ext=ogg] / bestaudio[ext=mp3]' --output '%(album_artist,artist|unknown artist)s/%(album,track|unknown album)s/%(track_number|0)s %(track,id,epoch)s.%(ext)s'";
+      dl-music-best = "${dl-music} --format 'bestaudio[ext=flac] / bestaudio[ext=ogg] / bestaudio[ext=mp3]'";
       dl-music-yt = "${dl-base} --format 'bestaudio' --extract-audio --audio-format opus";
       noansi = "sed \"s,\\x1B\\[[0-9;]*[a-zA-Z],,g\"";
       loggy = "${noansi} | tee ~/tmp/$(date +%F-%T)-log.txt";
