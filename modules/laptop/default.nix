65ac5c085e052b45deeccd84cfae285940010209
Stel Abrego <stel@stel.codes>
2023-04-11 23:30:16 -0700

    Refactor laptop module

--- modules/laptop/default.nix
+++ modules/laptop/default.nix
@@ -3,48 +3,113 @@
 
   config = {
 
-    hardware.bluetooth.enable = true;
-    hardware.opengl.enable = true;
-
-    # Need this for font-manager or any other gtk app to work I guess
-    programs.dconf.enable = true;
-
-    # Enable CUPS to print documents.
-    services.printing.enable = true;
-
-    # Configure keymap in X11
-    services.xserver = {
-      enable = true;
-      autorun = false;
-      layout = "us";
-      xkbVariant = "";
-      xkbOptions = "caps:swapescape";
-      libinput.enable = true;
-    };
-
-    # Enable iOS devices to automatically connect
-    # Use idevice* commands like ideviceinfo
-    services.usbmuxd.enable = true;
-
-    services.blueman.enable = true;
-    services.gnome.gnome-keyring.enable = true;
-
-    # Enable sound with pipewire.
     sound.enable = true;
-    hardware.pulseaudio.enable = false;
+
+    # Needed for pipewire
     security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      jack.enable = true;
+
+    hardware = {
+      bluetooth.enable = true;
+      opengl.enable = true;
+      # Use pipewire instead of pulseaudio
+      pulseaudio.enable = false;
     };
 
-    # don’t shutdown when power button is short-pressed
-    services.logind.extraConfig = "HandlePowerKey=hibernate";
-    services.logind.lidSwitch = "suspend-then-hibernate";
+    # Hibernate after 30 minutes of sleep
     systemd.sleep.extraConfig = "HibernateDelaySec=30m";
+
+    programs = {
+
+      # Need this for font-manager or any other gtk app to work I guess
+      dconf.enable = true;
+
+      sway.enable = true;
+
+      # https://github.com/mkaply/queryamoid/releases/tag/v0.1
+      # https://github.com/mozilla/policy-templates/blob/master/README.md#extensionsettings
+      # Apparently Mozilla doesn't let you set the default search engine using policies anymore >:c
+      firefox = {
+        enable = true;
+        preferences = {
+          "browser.chrome.toolbar_tips" = false;
+          "browser.uidensity" = 1;
+          "browser.fullscreen.autohide" = false;
+          "browser.tabs.insertAfterCurrent" = true;
+          "browser.newtabpage.enabled" = false;
+          "browser.startup.homepage" = "chrome://browser/content/blanktab.html";
+          "browser.shell.checkDefaultBrowser" = false;
+        };
+        policies = {
+          ExtensionSettings = {
+            "uBlock0@raymondhill.net" = {
+              installation_mode = "normal_installed";
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            };
+            "vimium-c@gdh1995.cn" = {
+              installation_mode = "normal_installed";
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-c/latest.xpi";
+            };
+          };
+        };
+      };
+
+    };
+
+    services = {
+
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+      };
+
+      # Enable CUPS to print documents.
+      printing.enable = true;
+
+      # Configure keymap in X11
+      xserver = {
+        enable = true;
+        autorun = false;
+        layout = "us";
+        xkbVariant = "";
+        xkbOptions = "caps:swapescape";
+        libinput.enable = true;
+      };
+
+      # Enable iOS devices to automatically connect
+      # Use idevice* commands like ideviceinfo
+      usbmuxd.enable = true;
+
+      blueman.enable = true;
+
+      gnome.gnome-keyring.enable = true;
+
+      # don’t shutdown when power button is short-pressed
+      logind.extraConfig = "HandlePowerKey=hibernate";
+      logind.lidSwitch = "suspend-then-hibernate";
+
+      cron = {
+        enable = true;
+        # https://crontab.guru
+        systemCronJobs =
+          let
+            hibernateCriticalBattery = pkgs.writeShellScript "hibernate-critical-battery" ''
+              ${pkgs.acpi}/bin/acpi -b | ${pkgs.gawk}/bin/awk -F'[,:%]' '{print $2, $3}' | {
+                read -r status capacity
+                if [ "$status" = Discharging -a "$capacity" -lt 8 ]; then
+                  ${pkgs.systemd}/bin/systemctl hibernate
+                fi
+              }
+            '';
+          in
+          [
+            "* * * * * ${hibernateCriticalBattery}"
+          ];
+      };
+    };
+
 
     powerManagement = {
       enable = true;
@@ -54,60 +119,13 @@
       # Maybe switch to services.tlp if I need configuration
     };
 
-    fonts.fontconfig.enable = true;
-    fonts.enableDefaultFonts = true;
-    fonts.fonts = [
-      # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/data/fonts/nerdfonts/shas.nix
-      (pkgs.nerdfonts.override { fonts = [ "Noto" "JetBrainsMono" ]; })
-    ];
-
-    # https://github.com/mkaply/queryamoid/releases/tag/v0.1
-    # https://github.com/mozilla/policy-templates/blob/master/README.md#extensionsettings
-    # Apparently Mozilla doesn't let you set the default search engine using policies anymore >:c
-    programs.firefox = {
-      enable = true;
-      preferences = {
-        "browser.chrome.toolbar_tips" = false;
-        "browser.uidensity" = 1;
-        "browser.fullscreen.autohide" = false;
-        "browser.tabs.insertAfterCurrent" = true;
-        "browser.newtabpage.enabled" = false;
-        "browser.startup.homepage" = "chrome://browser/content/blanktab.html";
-        "browser.shell.checkDefaultBrowser" = false;
-      };
-      policies = {
-        ExtensionSettings = {
-          "uBlock0@raymondhill.net" = {
-            installation_mode = "normal_installed";
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          };
-          "vimium-c@gdh1995.cn" = {
-            installation_mode = "normal_installed";
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-c/latest.xpi";
-          };
-        };
-      };
-    };
-
-    programs.sway.enable = true;
-
-    services.cron = {
-      enable = true;
-      # https://crontab.guru
-      systemCronJobs =
-        let
-          hibernateCriticalBattery = pkgs.writeShellScript "hibernate-critical-battery" ''
-            ${pkgs.acpi}/bin/acpi -b | ${pkgs.gawk}/bin/awk -F'[,:%]' '{print $2, $3}' | {
-              read -r status capacity
-              if [ "$status" = Discharging -a "$capacity" -lt 8 ]; then
-                ${pkgs.systemd}/bin/systemctl hibernate
-              fi
-            }
-          '';
-        in
-        [
-          "* * * * * ${hibernateCriticalBattery}"
-        ];
+    fonts = {
+      fontconfig.enable = true;
+      enableDefaultFonts = true;
+      fonts = [
+        # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/data/fonts/nerdfonts/shas.nix
+        (pkgs.nerdfonts.override { fonts = [ "Noto" "JetBrainsMono" ]; })
+      ];
     };
 
     environment.systemPackages = [
@@ -128,7 +146,6 @@
       pkgs.pavucontrol
       # pkgs.libsForQt5.qt5.qtwayland
       pkgs.tor-browser-bundle-bin
-      pkgs.acpi
     ];
 
   };
