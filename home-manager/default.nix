518ceb5769fe4bba20905d91d31eb4c631796da4
Stel Abrego <stel@stel.codes>
2023-04-16 17:38:27 -0700

    nnn: add plugins

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -149,7 +149,13 @@ pkgs: {
 
     nnn = {
       enable = true;
-      package = pkgs.nnn.override ({ withNerdIcons = true; });
+      package = pkgs.nnn.override { withNerdIcons = true; };
+      plugins.src = (pkgs.fetchFromGitHub {
+        owner = "jarun";
+        repo = "nnn";
+        rev = "v4.8";
+        sha256 = "QbKW2wjhUNej3zoX18LdeUHqjNLYhEKyvPH2MXzp/iQ=";
+      }) + "/plugins";
     };
 
 
