85a8c3d9a2b072580a0ffe958d1089f96b7df09f
Stel Abrego <stel@stel.codes>
2023-04-30 14:09:25 -0700

    sway: add mozilla wayland env var

--- home-manager/sway/default.nix
+++ home-manager/sway/default.nix
@@ -36,6 +36,7 @@ pkgs: {
       # Fix for some Java AWT applications (e.g. Android Studio),
       # use this if they aren't displayed properly:
       export _JAVA_AWT_WM_NONREPARENTING=1
+      export MOZ_ENABLE_WAYLAND=1
     '';
     config = rec {
       terminal = "${pkgs.kitty}/bin/kitty";
