68117fa64e2e71b26cce50dbda8c03081bbd5abc
Stel Abrego <stel@stel.codes>
2023-05-10 12:09:15 -0700

    firefox: major update to default policies

--- modules/laptop/default.nix
+++ modules/laptop/default.nix
@@ -43,25 +43,43 @@
           Preferences =
             let
               user = (x: { Status = "user"; Value = x; });
+              locked = (x: { Status = "locked"; Value = x; });
             in
             {
-              "browser.chrome.toolbar_tips" = user false;
-              "browser.uidensity" = user 1;
-              "browser.fullscreen.autohide" = user false;
-              "browser.tabs.insertAfterCurrent" = user true;
-              "browser.newtabpage.enabled" = user false;
-              "browser.startup.homepage" = user "chrome://browser/content/blanktab.html";
-              "browser.shell.checkDefaultBrowser" = user false;
+              "browser.chrome.toolbar_tips" = locked false;
+              "browser.uidensity" = locked 1;
+              "browser.fullscreen.autohide" = locked false;
+              "browser.tabs.insertAfterCurrent" = locked true;
+              "browser.urlbar.suggest.quicksuggest.nonsponsored" = locked false;
+              "browser.urlbar.suggest.quicksuggest.sponsored" = locked false;
+              "browser.urlbar.suggest.topsites" = locked false;
+              "browser.urlbar.suggest.calculator" = locked true;
+              "browser.urlbar.suggest.engines" = locked false;
+              "browser.urlbar.suggest.searches" = locked false;
+              "dom.security.https_only_mode" = locked true;
               "font.name.monospace.x-western" = user "NotoSansMono Nerd Font";
               "font.name.sans-serif.x-western" = user "NotoSans Nerd Font";
               "font.name.serif.x-western" = user "NotoSerif Nerd Font";
-              "media.ffmpeg.vaapi.enabled" = user true;
-              "extensions.pocket.enabled" = user false;
+              "media.ffmpeg.vaapi.enabled" = locked true;
             };
+          DisableFirefoxStudies = true;
+          DisablePocket = true;
+          DisableSetDesktopBackground = true;
+          DisableTelemetry = true;
+          DisplayBookmarksToolbar = "newtab";
+          DontCheckDefaultBrowser = true;
           DNSOverHTTPS = {
             # Solves my protonvpn reconnect dns issue
             Enabled = true;
             Locked = true;
+          };
+          EnableTrackingProtection = {
+            Value = true;
+            Locked = true;
+            Cryptomining = true;
+            Fingerprinting = true;
+            EmailTracking = true;
+            Exceptions = [ ];
           };
           ExtensionSettings = {
             # https://github.com/mkaply/queryamoid/releases/tag/v0.1
@@ -77,6 +95,23 @@
               installation_mode = "normal_installed";
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-c/latest.xpi";
             };
+          };
+          HardwareAcceleration = true;
+          Homepage = {
+            URL = "chrome://browser/content/blanktab.html";
+            Locked = false;
+            StartPage = "none";
+          };
+          NewTabPage = false;
+          OfferToSaveLogins = false;
+          OverrideFirstRunPage = "";
+          PasswordManagerEnabled = false;
+          WebsiteFilter = {
+            Block = [
+              "*://news.ycombinator.com/*"
+              "*://*.reddit.com/"
+              "*://*.reddit.com/r/popular/"
+            ];
           };
         };
       };
