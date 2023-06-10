d103511393dfd52d75b6ec37ab67ca6cd92d8002
Stel Abrego <stel@stel.codes>
2023-06-09 17:37:53 -0700

    cleanup: remove gnome and server modules

--- modules/gnome/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: {
-  environment.systemPackages = [ pkgs.gnome ];
-  environment.etc."gnome/.xinitrc" = ''
-    export XDG_SESSION_TYPE=x11
-    export GDK_BACKEND=x11
-    exec gnome-session
-  '';
-}
