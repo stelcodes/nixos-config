a1c8fa49f6d303b8711f3f6767c6102da67a012c
Stel Abrego <stel@stel.codes>
2023-06-17 18:31:18 -0700

    nix: stop warning about dirty git repos

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -217,6 +217,7 @@
         experimental-features = nix-command flakes
         keep-outputs          = true
         keep-derivations      = true
+        warn-dirty            = false
       '';
     };
 
