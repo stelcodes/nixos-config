629d79ebe204813f1ff7bcc8da3198150d8dbf86
Stel Abrego <stel@stel.codes>
2023-07-27 11:39:53 -0700

    flake.nix: create mkComputer function for nixosConfigurations

--- flake.nix
+++ flake.nix
@@ -26,16 +26,32 @@
   # Function that tells my flake which to use and what do what to do with the dependencies.
   outputs = inputs:
     let
-      # Variables that can be used in the config files.
-      user = "stel";
-      system = "x86_64-linux";
-      pkgs = import inputs.nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-        overlays = [ ];
-      };
-      themes = (import ./misc/themes.nix) pkgs;
-      theme = themes.everforest;
+      mkComputer = { system, user, themeName, hostName, ... }:
+        let
+          pkgs = import inputs.nixpkgs { inherit system; };
+          themes = import ./misc/themes.nix pkgs;
+          theme = themes.${themeName};
+        in
+        inputs.nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs user theme hostName;
+          };
+          modules = [
+            ./hosts/${hostName}
+            inputs.home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = {
+                inherit inputs user theme hostName;
+              };
+              home-manager.users.${user} = {
+                imports = [ ./hosts/${hostName}/home.nix ];
+              };
+            }
+          ];
+        };
     in
     {
       nixosConfigurations = {
@@ -43,92 +59,32 @@
         ########################################################################
         # framework laptop i5-1240P
         ########################################################################
-        framework = inputs.nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = {
-            inherit inputs user theme;
-            hostName = "framework";
-          };
-          modules = [
-            ./hosts/framework
-            inputs.home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = {
-                inherit inputs user theme;
-                hostName = "framework";
-                mainMonitor = "eDP-1";
-              };
-              home-manager.users.${user} = {
-                imports = [
-                  ./hosts/framework/home.nix
-                ];
-              };
-            }
-          ];
+        framework = mkComputer {
+          hostName = "framework";
+          system = "x86_64-linux";
+          themeName = "everforest";
+          user = "stel";
         };
-
 
         ########################################################################
         # desktop fractal meshify 2 w ryzen 5600x
         ########################################################################
-        meshify =
-          let
-            hostName = "meshify";
-          in
-          inputs.nixpkgs.lib.nixosSystem {
-            inherit system;
-            specialArgs = {
-              inherit inputs user theme hostName;
-            };
-            modules = [
-              ./hosts/${hostName}
-              inputs.home-manager.nixosModules.home-manager
-              {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.extraSpecialArgs = {
-                  inherit inputs user theme hostName;
-                };
-                home-manager.users.${user} = {
-                  imports = [ ./hosts/${hostName}/home.nix ];
-                };
-              }
-            ];
-          };
+        meshify = mkComputer {
+          hostName = "meshify";
+          system = "x86_64-linux";
+          themeName = "everforest";
+          user = "stel";
+        };
 
 
         ########################################################################
         # macbook laptop i7-4650U
         ########################################################################
-        macbook = inputs.nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = {
-            inherit inputs user;
-            hostName = "macbook";
-          };
-          modules = [
-            ./hosts/macbook
-            ./modules/common
-            ./modules/graphical
-            ./modules/laptop
-            inputs.home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = {
-                inherit user;
-                hostName = "macbook";
-                mainMonitor = "eDP-1";
-              };
-              home-manager.users.${user} = {
-                imports = [
-                  ./modules/laptop/home.nix
-                ];
-              };
-            }
-          ];
+        macbook = mkComputer {
+          hostName = "meshify";
+          system = "x86_64-linux";
+          themeName = "everforest";
+          user = "stel";
         };
 
       };
