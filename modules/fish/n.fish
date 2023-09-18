ced4b92a331bff603e9095d5fcc309de3a79cb15
Stel Abrego <stel@stel.codes>
2023-09-18 14:24:15 -0700

    fish: improve n function for invoking nnn

--- modules/fish/n.fish
+++ modules/fish/n.fish
@@ -4,6 +4,7 @@ if set --query NNNLVL
 end
 
 set -x NNN_TMPFILE $(mktemp)
+set -x NNN_SEL $(mktemp)
 
 if test $TERM_PROGRAM = "tmux"
   command nnn -eauUAP p $argv
@@ -12,6 +13,10 @@ else
 end
 
 if test -e $NNN_TMPFILE
-    source $NNN_TMPFILE
+    cd $(string sub --start 5 --end -1 < $NNN_TMPFILE)
     rm $NNN_TMPFILE
+end
+
+if test -e $NNN_SEL
+    rm $NNN_SEL
 end
