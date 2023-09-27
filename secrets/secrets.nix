f41dbff46904bb23e7af6666672acd98740d3927
Stel Abrego <stel@stel.codes>
2023-09-27 11:31:54 -0700

    refactor: make secrets.nix more readable

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,28 +1,17 @@
 let
-  users = {
-    stel-framework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGl9G7SYvJy8+u2AF+Mlez6bwhrNfKclWo9mK6mwtNgJ";
+  adminKeys = {
+    framework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGl9G7SYvJy8+u2AF+Mlez6bwhrNfKclWo9mK6mwtNgJ";
   };
-  systems = {
+  systemKeys = {
     meshify = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHnPEH3EdHpXZxp4yfD2/psm1m8dbHSGnQ95NLPf6S5g";
     framework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPv1nZsr+fdrSgtCOrYfsR0c+a3iOdaPJQHEWpZ44xXJ";
   };
-  allSystems = builtins.attrValues systems;
-  allUsers = builtins.attrValues users;
-  allEntities = allSystems ++ allUsers;
+  allAdminKeys = builtins.attrValues adminKeys;
+  allSystemKeys = builtins.attrValues systemKeys;
+  allKeys = allAdminKeys ++ allSystemKeys;
 in
 {
-  # For all systems use `builtins.attrValues systems`
-
-  "meshify/wg/pvpn-fast/private-key.age".publicKeys = [ systems.meshify ];
-  "meshify/wg/pvpn-fast/public-key.age".publicKeys = [ systems.meshify ];
-  "meshify/wg/pvpn-fast/endpoint.age".publicKeys = [ systems.meshify ];
-
-  "meshify/wg/pvpn-secure/private-key.age".publicKeys = [ systems.meshify ];
-  "meshify/wg/pvpn-secure/public-key.age".publicKeys = [ systems.meshify ];
-  "meshify/wg/pvpn-secure/endpoint.age".publicKeys = [ systems.meshify ];
-
-  "framework-pvpn-fast-wg-quick-config.age".publicKeys = [ systems.framework ];
-
-  "admin-password.age".publicKeys = allEntities;
-  "root-password.age".publicKeys = allEntities;
+  "framework-pvpn-fast-wg-quick-config.age".publicKeys = [ systemKeys.framework ] ++ allAdminKeys;
+  "admin-password.age".publicKeys = allKeys;
+  "root-password.age".publicKeys = allKeys;
 }
