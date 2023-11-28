1dabac7e985d66b3ed2660d5cbce049e0ae9deb7
Stel Abrego <stel@stel.codes>
2023-11-27 23:12:30 -0800

    Don't start nnn preview automatically

--- modules/fish/n.fish
+++ modules/fish/n.fish
@@ -6,11 +6,11 @@ end
 set -x NNN_TMPFILE $(mktemp)
 set -x NNN_SEL $(mktemp)
 
-if test "$TERM_PROGRAM" = "tmux"
-  command nnn -eauUAGP p $argv
-else
-  command nnn -eauUAG $argv
-end
+# I don't actually like when preview is on by default
+# if test "$TERM_PROGRAM" = "tmux"
+#   command nnn -eauUAGP p $argv
+
+command nnn -eauUAG $argv
 
 if test -e "$NNN_TMPFILE"
     cd $(string sub --start 5 --end -1 < $NNN_TMPFILE)
