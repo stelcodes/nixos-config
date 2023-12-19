41bdc86b8a72ca96c9a885d87a5b160e7cdb7889
Stel Abrego <stel@stel.codes>
2023-12-18 16:08:32 -0800

    Complete final steps to build kairi image, add docs

--- flake.nix
+++ flake.nix
@@ -126,11 +126,20 @@
         ########################################################################
         # digital ocean droplet
         ########################################################################
+        # build vm image:
+        #   nixos-rebuild build-vm-with-bootloader --flake "$HOME/nixos-config#kairi"
+        # test ssh:
+        #   QEMU_NET_OPTS='hostfwd=tcp::2222-:22' <vm_start_script>
+        #   ssh stel@localhost -p 2222
+        # build droplet image:
+        #   nix build .#nixosConfigurations.kairi.config.formats.do
         kairi = mkComputer {
           hostName = "kairi";
           system = "x86_64-linux";
           profile = {
             minimal = true;
+            server = true;
+            virtual = true;
           };
         };
         ########################################################################
