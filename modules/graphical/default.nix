228c23179c9b3aa59cf97a0115ee5f07e60ecb95
Stel Abrego <stel@stel.codes>
2023-12-20 15:03:14 -0800

    Remove vimium c and darkreader firefox extensions

--- modules/graphical/default.nix
+++ modules/graphical/default.nix
@@ -103,19 +103,10 @@
               installation_mode = "normal_installed";
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
             };
-            # "vimium-c@gdh1995.cn" = {
-            #   installation_mode = "normal_installed";
-            #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-c/latest.xpi";
-            # };
             "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
               installation_mode = "normal_installed";
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
             };
-            # "addon@darkreader.org" = {
-            #   # brightness 35, contrast -5
-            #   installation_mode = "normal_installed";
-            #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
-            # };
             "{73a6fe31-595d-460b-a920-fcc0f8843232}" = {
               installation_mode = "normal_installed";
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/noscript/latest.xpi";
