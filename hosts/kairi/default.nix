35615514c06d97d4448796d8f52ec6ea305e4d12
Stel Abrego <stel@stel.codes>
2024-01-18 16:56:13 -0800

    Update kairi config

--- hosts/kairi/default.nix
+++ hosts/kairi/default.nix
@@ -1,16 +1,12 @@
 { ... }: {
 
-  # build vm image:
-  #   nixos-rebuild build-vm-with-bootloader --flake "$HOME/nixos-config#kairi"
-  # test ssh:
-  #   QEMU_NET_OPTS='hostfwd=tcp::2222-:22' <vm_start_script>
-  #   ssh stel@localhost -p 2222
-  # build droplet image:
-  #   nix build .#nixosConfigurations.kairi.config.formats.do
-
   profile = {
+    audio = true;
+    graphical = true;
+    battery = false;
     server = true;
-    virtual = true;
+    virtual = false;
+    virtualHost = false;
   };
 
   system.stateVersion = "23.11";
