aebe371ff6f4f37ab38d392c14ecc893c600d2e9
Stel Abrego <stel@stel.codes>
2024-01-18 16:29:21 -0800

    Update meshify packages

--- hosts/meshify/home.nix
+++ hosts/meshify/home.nix
@@ -3,15 +3,19 @@
   home = {
     stateVersion = "23.05";
     packages = [
+      pkgs.obsidian
+      pkgs.gimp-with-plugins
+      pkgs.gajim
+      pkgs.signal-desktop
       pkgs.fractal
       inputs.gpt4all-nix.packages.${pkgs.system}.default
       inputs.arcsearch.defaultPackage.${pkgs.system}
+      pkgs.retroarch-loaded
+      pkgs.kodi-loaded
+      pkgs.discord-firefox
     ];
   };
   wayland.windowManager.sway.config.workspaceOutputAssign = [
-    {
-      output = "HDMI-A-1";
-      workspace = "5";
-    }
+    { output = "HDMI-A-1"; workspace = "5"; }
   ];
 }
