4c88c0aff72c12e916a19bc68047415712c493bd
Stel Abrego <stel@stel.codes>
2024-01-18 16:28:48 -0800

    Make progress on olette setup

--- hosts/olette/default.nix
+++ hosts/olette/default.nix
@@ -1,12 +1,38 @@
-{pkgs, ... }: {
+{ pkgs, ... }: {
+
+  system.stateVersion = "23.11";
 
   profile = {
-    server = true;
+    audio = true;
     battery = false;
-    graphical = false;
+    bluetooth = true;
+    graphical = true;
+    server = true;
     virtual = false;
     virtualHost = false;
   };
 
-  system.stateVersion = "23.11";
+  hardware = {
+    bluetooth.enable = true;
+    opengl.enable = true;
+    xpadneo.enable = true;
+  };
+
+  age.secrets = {
+    root-password.file = ../../secrets/root-password.age;
+    admin-password.file = ../../secrets/admin-password.age;
+  };
+
+  # Uncomment this when secrets are rekeyed with new system ssh key
+  # users.users = {
+  #   root.hashedPasswordFile = config.age.secrets.root-password.path;
+  #   ${config.admin.username}.hashedPasswordFile = config.age.secrets.admin-password.path;
+  # };
+
+  services = {
+    getty.autoLoginUser = "${config.admin.username}";
+    syncthing = {
+      enable = true;
+    };
+  };
 }
