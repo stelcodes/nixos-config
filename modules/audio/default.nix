0b8dd95f30c8312e271f4ab942d2ce649d6ec4d5
Stel Abrego <stel@stel.codes>
2024-02-04 17:52:16 -0800

    Fix totally broken audio because of incorrect merge of mkIf

--- modules/audio/default.nix
+++ modules/audio/default.nix
@@ -31,8 +31,49 @@
 
   config =
     let
+      baseConfig = {
+
+        sound.enable = true;
+
+        # Needed for pipewire
+        security.rtkit.enable = true;
+
+        hardware = {
+          # Use pipewire instead of pulseaudio
+          pulseaudio.enable = false;
+        };
+
+        services.pipewire = {
+          enable = true;
+          alsa.enable = true;
+          alsa.support32Bit = true;
+          pulse.enable = true;
+          jack.enable = true;
+        };
+
+        environment.variables =
+          let
+            makePluginPath = format:
+              # Yabridge uses the first entry for installation directory so it needs to be writable
+              "$HOME/.${format}:" +
+              (lib.makeSearchPath format [
+                "$HOME/.nix-profile/lib"
+                "/run/current-system/sw/lib"
+                "/etc/profiles/per-user/$USER/lib"
+              ]);
+          in
+          lib.mkForce {
+            DSSI_PATH = makePluginPath "dssi";
+            LADSPA_PATH = makePluginPath "ladspa";
+            LV2_PATH = makePluginPath "lv2";
+            LXVST_PATH = makePluginPath "lxvst";
+            VST_PATH = makePluginPath "vst";
+            VST3_PATH = makePluginPath "vst3";
+            CLAP_PATH = makePluginPath "clap";
+          };
+
+      };
       realtimeConfig = {
-        # https://github.com/mixxxdj/mixxx/wiki/Adjusting-Audio-Latency
 
         services.tlp.settings = {
           RUNTIME_PM_DISABLE = config.sound.realtime.soundcardPciId;
@@ -53,51 +94,12 @@
 
       };
     in
-    lib.mkIf config.profile.audio ({
-
-      sound.enable = true;
-
-      # Needed for pipewire
-      security.rtkit.enable = true;
-
-      hardware = {
-        # Use pipewire instead of pulseaudio
-        pulseaudio.enable = false;
-      };
-
-      services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-        jack.enable = true;
-      };
-
-      environment.variables =
-        let
-          makePluginPath = format:
-            # Yabridge uses the first entry for installation directory so it needs to be writable
-            "$HOME/.${format}:" +
-            (lib.makeSearchPath format [
-              "$HOME/.nix-profile/lib"
-              "/run/current-system/sw/lib"
-              "/etc/profiles/per-user/$USER/lib"
-            ]);
-        in
-        lib.mkForce {
-          DSSI_PATH = makePluginPath "dssi";
-          LADSPA_PATH = makePluginPath "ladspa";
-          LV2_PATH = makePluginPath "lv2";
-          LXVST_PATH = makePluginPath "lxvst";
-          VST_PATH = makePluginPath "vst";
-          VST3_PATH = makePluginPath "vst3";
-          CLAP_PATH = makePluginPath "clap";
-        };
-
-      specialisation = lib.mkIf (config.sound.realtime.enable && config.sound.realtime.specialisation)
-        {
-          realtime-audio.configuration = realtimeConfig;
-        };
-    } // (lib.mkIf (config.sound.realtime.enable && !config.sound.realtime.specialisation) realtimeConfig ));
+    lib.mkIf config.profile.audio (lib.mkMerge [
+      baseConfig
+      (lib.mkIf (config.sound.realtime.enable && !config.sound.realtime.specialisation) realtimeConfig)
+      (lib.mkIf (config.sound.realtime.enable && config.sound.realtime.specialisation) {
+        specialisation.realtime-audio.configuration = realtimeConfig;
+      })
+    ]);
 
 }
