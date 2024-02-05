3e780d08158839e25c11e6c382e1e89a7f1ddb1d
Stel Abrego <stel@stel.codes>
2024-02-04 16:48:17 -0800

    Update audio module realtime settings

--- modules/audio/default.nix
+++ modules/audio/default.nix
@@ -9,6 +9,11 @@
     sound.realtime = {
       enable = lib.mkEnableOption "Enable realtime specialisation";
 
+      specialisation = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+      };
+
       soundcardPciId = lib.mkOption {
         description = "Find with lspci | grep Audio";
         type = lib.types.str;
@@ -24,102 +29,14 @@
 
   };
 
-  config = lib.mkIf config.profile.audio {
-
-    sound.enable = true;
-
-    # Needed for pipewire
-    security.rtkit.enable = true;
-
-    hardware = {
-      # Use pipewire instead of pulseaudio
-      pulseaudio.enable = false;
-    };
-
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      jack.enable = true;
-    };
-
-    environment.variables =
-      let
-        makePluginPath = format:
-          # Yabridge uses the first entry for installation directory so it needs to be writable
-          "$HOME/.${format}:" +
-          (lib.makeSearchPath format [
-            "$HOME/.nix-profile/lib"
-            "/run/current-system/sw/lib"
-            "/etc/profiles/per-user/$USER/lib"
-          ]);
-      in
-      {
-        DSSI_PATH = makePluginPath "dssi";
-        LADSPA_PATH = makePluginPath "ladspa";
-        LV2_PATH = makePluginPath "lv2";
-        LXVST_PATH = makePluginPath "lxvst";
-        VST_PATH = makePluginPath "vst";
-        VST3_PATH = makePluginPath "vst3";
-        CLAP_PATH = makePluginPath "clap";
-      };
-
-    specialisation = lib.mkIf config.sound.realtime.enable {
-
-      realtime-audio.configuration = {
-
+  config =
+    let
+      realtimeConfig = {
         # https://github.com/mixxxdj/mixxx/wiki/Adjusting-Audio-Latency
-        boot.kernelParams = [ "nosmt" ];
 
         services.tlp.settings = {
-          RUNTIME_PM_DISABLE = config.sound.soundcardPciId;
+          RUNTIME_PM_DISABLE = config.sound.realtime.soundcardPciId;
         };
-
-
-        environment.etc =
-          let
-            quant = builtins.toString config.sound.realtime.quant;
-            json = pkgs.formats.json { };
-          in
-          {
-            # NOTE: Every setup is different, and a lot of factors determine your final
-            # latency, like CPU speed, RT/PREEMPTIVE kernels and soundcards supporting
-            # different audio formats. That's why 32/48000 isn't always a value that's
-            # going to work for everyone. The best way to get everything working is to
-            # keep increasing the quant value until you get no crackles (underruns) or
-            # until you get audio again (in case there wasn't any). This won't
-            # guarantee the lowest possible latency, but will provide a decent one
-            # paired with stable audio.
-            # default.clock.quantum = 32
-            # https://nixos.wiki/wiki/PipeWire
-            "pipewire/pipewire.conf.d/92-low-latency.conf".text = ''
-              context.properties = {
-                default.clock.rate = 48000
-                default.clock.quantum = ${quant}
-                default.clock.min-quantum = ${quant}
-                default.clock.max-quantum = ${quant}
-              }
-            '';
-            "pipewire/pipewire-pulse.d/92-low-latency.conf".source = json.generate "92-low-latency.conf" {
-              context.modules = [
-                {
-                  name = "libpipewire-module-protocol-pulse";
-                  args = {
-                    pulse.min.req = "${quant}/48000";
-                    pulse.default.req = "${quant}/48000";
-                    pulse.max.req = "${quant}/48000";
-                    pulse.min.quantum = "${quant}/48000";
-                    pulse.max.quantum = "${quant}/48000";
-                  };
-                }
-              ];
-              stream.properties = {
-                node.latency = "${quant}/48000";
-                resample.quality = 1;
-              };
-            };
-          };
 
         # https://github.com/musnix/musnix
         musnix = {
@@ -135,7 +52,52 @@
         };
 
       };
-    };
-  };
+    in
+    lib.mkIf config.profile.audio ({
+
+      sound.enable = true;
+
+      # Needed for pipewire
+      security.rtkit.enable = true;
+
+      hardware = {
+        # Use pipewire instead of pulseaudio
+        pulseaudio.enable = false;
+      };
+
+      services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+      };
+
+      environment.variables =
+        let
+          makePluginPath = format:
+            # Yabridge uses the first entry for installation directory so it needs to be writable
+            "$HOME/.${format}:" +
+            (lib.makeSearchPath format [
+              "$HOME/.nix-profile/lib"
+              "/run/current-system/sw/lib"
+              "/etc/profiles/per-user/$USER/lib"
+            ]);
+        in
+        lib.mkForce {
+          DSSI_PATH = makePluginPath "dssi";
+          LADSPA_PATH = makePluginPath "ladspa";
+          LV2_PATH = makePluginPath "lv2";
+          LXVST_PATH = makePluginPath "lxvst";
+          VST_PATH = makePluginPath "vst";
+          VST3_PATH = makePluginPath "vst3";
+          CLAP_PATH = makePluginPath "clap";
+        };
+
+      specialisation = lib.mkIf (config.sound.realtime.enable && config.sound.realtime.specialisation)
+        {
+          realtime-audio.configuration = realtimeConfig;
+        };
+    } // (lib.mkIf (config.sound.realtime.enable && !config.sound.realtime.specialisation) realtimeConfig ));
 
 }
