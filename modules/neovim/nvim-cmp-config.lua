34be4bea34b0fbdb5e43253198fea6c445d63b40
Stel Abrego <stel@stel.codes>
2024-02-17 15:09:48 -0800

    Fix neovim completion scrolling

--- modules/neovim/nvim-cmp-config.lua
+++ modules/neovim/nvim-cmp-config.lua
@@ -18,10 +18,8 @@ cmp.setup {
     -- documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert {
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    ['<C-j>'] = cmp.mapping.scroll_docs(3),
+    ['<C-k>'] = cmp.mapping.scroll_docs(-3),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
