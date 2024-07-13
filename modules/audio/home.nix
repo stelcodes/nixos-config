f96ba8a89a9110dc058d8072c46874f06aea056f
Stel Abrego <stel@stel.codes>
2024-07-12 18:48:56 -0700

    Start using bitwig from unstable packages

--- modules/audio/home.nix
+++ modules/audio/home.nix
@@ -3,13 +3,14 @@
   home = {
     packages = (lib.lists.optionals systemConfig.activities.jamming [
       # inputs.audio-nix.packages.${pkgs.system}.bitwig-studio5-latest
-      pkgs.bitwig-studio
+      pkgs.unstable.bitwig-studio
       pkgs.musescore
       # pkgs.lsp-plugins
       pkgs.graillon-free
       pkgs.yabridge
       pkgs.yabridgectl
       pkgs.wineWowPackages.stagingFull
+      pkgs.winetricks
       pkgs.oxefmsynth
       pkgs.surge
       pkgs.surge-XT
