b2e560c12f173788a0eb3901781cccc72101fe3a
Stel Abrego <stel@stel.codes>
2024-08-26 19:21:20 -0700

    Turn beatrix into home server

--- hosts/beatrix/default.nix
+++ hosts/beatrix/default.nix
@@ -3,7 +3,6 @@
   imports = [
     ./hardware-configuration.nix
     inputs.nixos-hardware.nixosModules.common-cpu-intel
-    inputs.nixos-hardware.nixosModules.common-cpu-intel-sandy-bridge
   ];
 
   system.stateVersion = "23.11";
@@ -16,6 +15,8 @@
     virtual = false;
     virtualHost = false;
   };
+
+  activities.coding = true;
 
   hardware = {
     xpadneo.enable = true;
@@ -31,16 +32,84 @@
   #   ${config.admin.username}.hashedPasswordFile = config.age.secrets.admin-password.path;
   # };
 
+  services = {
+    samba = {
+      # https://nixos.wiki/wiki/Samba
+      # smbpasswd -a my_user
+      enable = true;
+      securityType = "user";
+      openFirewall = true;
+      extraConfig = /* ini */ ''
+        # Guests are disabled by default
+        server string = smbnix
+        netbios name = smbnix
+        # Speed increase?
+        use sendfile = yes
+        hosts allow = 192.168.0. 192.168.1. 127.0.0.1 localhost marlene.local
+        hosts deny = 0.0.0.0/0
+      '';
+      shares = {
+        private = {
+          # path = "/var/lib/samba/private";
+          path = "/run/media/archive";
+          browseable = "yes"; # default
+          "read only" = "yes";
+          "guest ok" = "no"; # default
+          "create mask" = "0644";
+          "directory mask" = "0755"; # default
+          "force user" = "stel";
+          "force group" = "users";
+        };
+      };
+    };
+
+    samba-wsdd = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    nfs.server = {
+      # Don't forget to open firewall
+      # allowedTCPPorts = [ 2049 ];
+      # allowedUDPPorts = [ 2049 ];
+      enable = true;
+      # https://www.man7.org/linux/man-pages/man5/exports.5.html
+      lockdPort = 4001;
+      mountdPort = 4002;
+      statdPort = 4000;
+      exports = ''
+        /nfs 192.168.1.101(rw,fsid=root,no_subtree_check)
+        /nfs/archive 192.168.1.101(ro,nohide,no_subtree_check,all_squash,anonuid=1000,anongid=1000)
+      '';
+    };
+  };
+
+  networking = {
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 111 2049 4000 4001 4002 ];
+      allowedUDPPorts = [ 111 2049 4000 4001 4002 ];
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /nfs 0755 root root -"
+  ];
+
   fileSystems = {
+    "/nfs/archive" = {
+      device = "/run/media/archive";
+      options = [ "bind" "nofail" "noatime" ];
+    };
     "/run/media/archive" = {
       device = "/dev/disk/by-uuid/fabb5a38-c104-4e34-8652-04864df28799";
       fsType = "btrfs";
-      options = [ "nofail" ];
+      options = [ "nofail" "noatime" ];
     };
   };
 
   services = {
-    getty.autologinUser = "${config.admin.username}";
+    # getty.autologinUser = "${config.admin.username}";
     syncthing.enable = true;
   };
 }
--- hosts/beatrix/home.nix
+++ hosts/beatrix/home.nix
@@ -4,39 +4,17 @@
     packages = [
       pkgs.kodi-loaded
       pkgs.retroarch-loaded
-      pkgs.bitwig-studio
     ];
   };
-
-  programs.fish.loginShellInit = ''
-    exec systemd-cat --identifier=sway sway
-  '';
 
   services.wlsunset.systemdTarget = "null.target";
 
   wayland.windowManager.sway = {
-    config = {
-      startup = [
-        { command = "${pkgs.kodi-loaded}/bin/kodi"; }
-        { command = "${pkgs.retroarch-loaded}/bin/retroarch"; }
-      ];
-      workspaceOutputAssign = [
-        # Hopefully this works? Might be HDMI-A-1
-        { output = "*"; workspace = "5"; }
-      ];
-    };
     sleep = {
       preferredType = "suspend";
       lockBefore = false;
-      auto = {
-        enable = true;
-        idleMinutes = 30;
-      };
+      auto.enable = false;
     };
     wallpaper = pkgs.wallpaper.anime-girl-cat;
-    extraConfig = ''
-      for_window [app_id=org.libretro.RetroArch] fullscreen disable
-      for_window [class=Kodi] fullscreen disable
-    '';
   };
 }
