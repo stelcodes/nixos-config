a858438ecf1094e4f4ab5982ba7aba5336858569
Stel Abrego <stel@stel.codes>
2024-08-30 16:42:49 -0700

    Fix marlene ssh public key

--- secrets/keys.nix
+++ secrets/keys.nix
@@ -3,7 +3,7 @@ rec {
     yuffie = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGl9G7SYvJy8+u2AF+Mlez6bwhrNfKclWo9mK6mwtNgJ";
     terra = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJkPXakQYSkH4hp9Zmm1ewMYusc8RlUaQQnQsx2wHPpn";
     aerith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA5n9utlRl7eF5lY4CrcsOmg19KtauYjlIooR73Ir7AW";
-    marlene = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAdsDYwclY5FFU4uEFYFOPHaDwmDpMDEmRtukQeLaY0V";
+    marlene = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIUIrkV61xmxSAGQLMatmK0hzPvp+Iekq74pW/Weep9a";
   };
   systemKeys = {
     terra = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHnPEH3EdHpXZxp4yfD2/psm1m8dbHSGnQ95NLPf6S5g";
