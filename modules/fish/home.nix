0c5faa2b5be0c27fa261c732fae4a419f5adc9fd
Stel Abrego <stel@stel.codes>
2024-08-31 17:31:41 -0700

    Fix yazi fish function

--- modules/fish/home.nix
+++ modules/fish/home.nix
@@ -126,14 +126,12 @@
     };
     functions = {
       yazi = /* fish */ ''
-        function yy
-          set tmp (mktemp -t "yazi-cwd.XXXXXX")
-          yazi $argv --cwd-file="$tmp"
-          if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-            cd -- "$cwd"
-          end
-          rm -f -- "$tmp"
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        yazi $argv --cwd-file="$tmp"
+        if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+          cd -- "$cwd"
         end
+        rm -f -- "$tmp"
       '';
       nnn = /* fish */ ''
         if set --query NNNLVL
