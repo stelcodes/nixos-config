b3c66b9fefe2847d2b8fc9693d3960d6b6bc2b89
Stel Clementine <dev@stelclementine.com>
2024-11-14 18:10:38 -0800

    Tweak mpv-unify, format with ruff

--- packages/mpv-unify/default.nix
+++ packages/mpv-unify/default.nix
@@ -1,2 +1,13 @@
 { writers }:
-writers.writePython3 "mpv-unify" { } (builtins.readFile ./mpv-unify.py)
+writers.writePython3Bin "mpv-unify"
+{
+  libraries = [ ];
+  # doCheck = true; # This will get released in 24.11
+  # flake8 error codes: https://flake8.pycqa.org/en/latest/user/error-codes.html
+  flakeIgnore = [
+    "E501" # line length
+  ];
+}
+  (builtins.readFile ./mpv-unify.py)
+
+# See https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/writers/scripts.nix#L1038
--- packages/mpv-unify/mpv-unify.py
+++ packages/mpv-unify/mpv-unify.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env nix-shell
-#!nix-shell -i python3
-
 import os
 import socket
 import errno
@@ -10,17 +7,17 @@ import shlex
 import argparse
 
 parser = argparse.ArgumentParser(
-    prog='mpv-unify',
-    description='Play files within a single instance of mpv',
-    epilog='Inspired by umpv script from mpv: https://github.com/mpv-player/mpv/blob/master/TOOLS/umpv')
+    prog="mpv-unify",
+    description="Play files within a single instance of mpv",
+    epilog="Inspired by umpv script from mpv: https://github.com/mpv-player/mpv/blob/master/TOOLS/umpv",
+)
 
-parser.add_argument('-a', '--append', action='store_true')
-parser.add_argument('-f', '--focus', action='store_true')
-parser.add_argument('files', nargs='+')
+parser.add_argument("-q", "--queue", action="store_true")
+parser.add_argument("-f", "--focus", action="store_true")
+parser.add_argument("files", nargs="+")
 
 args = parser.parse_args()
 
-print(args)
 
 # this is the same method mpv uses to decide this
 def is_url(filename):
@@ -28,21 +25,26 @@ def is_url(filename):
     if len(parts) < 2:
         return False
     # protocol prefix has no special characters => it's an URL
-    allowed_symbols = string.ascii_letters + string.digits + '_'
+    allowed_symbols = string.ascii_letters + string.digits + "_"
     prefix = parts[0]
     return all(map(lambda c: c in allowed_symbols, prefix))
+
 
 # make them absolute; also makes them safe against interpretation as options
 def make_abs(filename):
     if not is_url(filename):
         return os.path.abspath(filename)
     return filename
+
+
 files = (make_abs(f) for f in args.files)
 
 socket_dir = os.getenv("XDG_RUNTIME_DIR") or os.getenv("HOME")
 
 if not socket_dir:
-    raise ValueError("Either XDG_RUNTIME_DIR or HOME environment variable needs to be set")
+    raise ValueError(
+        "Either XDG_RUNTIME_DIR or HOME environment variable needs to be set"
+    )
 
 socket_path = os.path.join(socket_dir, ".mpv_unify_socket")
 
@@ -56,17 +58,17 @@ except socket.error as e:
         pass  # abandoned socket
     elif e.errno == errno.ENOENT:
         sock = None
-        pass # doesn't exist
+        pass  # doesn't exist
     else:
         raise e
 
 if sock:
     # Unhandled race condition: what if mpv is terminating right now?
-    verb = "append-play" if args.append else "replace"
+    verb = "append-play" if args.queue else "replace"
     for f in files:
         # escape: \ \n "
-        f = f.replace("\\", "\\\\").replace("\"", "\\\"").replace("\n", "\\n")
-        f = "\"" + f + "\""
+        f = f.replace("\\", "\\\\").replace('"', '\\"').replace("\n", "\\n")
+        f = '"' + f + '"'
         sock.send(("raw loadfile " + f + " " + verb + "\n").encode("utf-8"))
 else:
     # Let mpv recreate socket if it doesn't already exist.
@@ -75,8 +77,9 @@ else:
         opts.extend(["--focus-on-open"])
     else:
         opts.extend(["--no-focus-on-open"])
-    opts.extend(["--no-terminal", "--force-window", "--input-ipc-server=" + socket_path,
-                 "--"])
+    opts.extend(
+        ["--no-terminal", "--force-window", "--input-ipc-server=" + socket_path, "--"]
+    )
     opts.extend(files)
 
     # Popen instead of check_call to start child process and exit immediately
