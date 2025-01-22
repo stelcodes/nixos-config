f2af9dfaa44e1fbc8bf9f1184f295fdc9a118158
Stel Clementine <dev@stelclementine.com>
2025-01-22 08:22:45 -0800

    Update mpv-unify script

--- packages/mpv-unify/default.nix
+++ packages/mpv-unify/default.nix
@@ -1,13 +1,15 @@
-{ writers }:
+{ writers, mpv, replaceVars }:
 writers.writePython3Bin "mpv-unify"
 {
   libraries = [ ];
-  # doCheck = true; # This will get released in 24.11
+  doCheck = true; # This will get released in 24.11
   # flake8 error codes: https://flake8.pycqa.org/en/latest/user/error-codes.html
   flakeIgnore = [
     "E501" # line length
   ];
 }
-  (builtins.readFile ./mpv-unify.py)
+  (replaceVars ./mpv-unify.py {
+    mpv = "${mpv}/bin/mpv";
+  })
 
 # See https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/writers/scripts.nix#L1038
--- packages/mpv-unify/mpv-unify.py
+++ packages/mpv-unify/mpv-unify.py
@@ -5,6 +5,9 @@ import subprocess
 import string
 import shlex
 import argparse
+import sys
+import io
+import selectors
 
 parser = argparse.ArgumentParser(
     prog="mpv-unify",
@@ -17,6 +20,51 @@ parser.add_argument("-f", "--focus", action="store_true")
 parser.add_argument("files", nargs="+")
 
 args = parser.parse_args()
+
+
+def capture_subprocess_output(subprocess_args):
+    # Start subprocess
+    # bufsize = 1 means output is line buffered
+    # universal_newlines = True is required for line buffering
+    process = subprocess.Popen(
+        subprocess_args,
+        bufsize=1,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        universal_newlines=True,
+    )
+
+    # Create callback function for process output
+    buf = io.StringIO()
+
+    def handle_output(stream, mask):
+        # Because the process' output is line buffered, there's only ever one
+        # line to read when this function is called
+        line = stream.readline()
+        buf.write(line)
+        sys.stdout.write(line)
+
+    # Register callback for an "available for read" event from subprocess' stdout stream
+    selector = selectors.DefaultSelector()
+    selector.register(process.stdout, selectors.EVENT_READ, handle_output)
+
+    # Loop until subprocess is terminated
+    while process.poll() is None:
+        # Wait for events and handle them with their registered callbacks
+        events = selector.select()
+        for key, mask in events:
+            callback = key.data
+            callback(key.fileobj, mask)
+
+    # Get process return code
+    return_code = process.wait()
+    selector.close()
+
+    # Store buffered output
+    output = buf.getvalue()
+    buf.close()
+
+    return (return_code, output)
 
 
 # this is the same method mpv uses to decide this
@@ -72,11 +120,11 @@ if sock:
         sock.send(("raw loadfile " + f + " " + verb + "\n").encode("utf-8"))
 else:
     # Let mpv recreate socket if it doesn't already exist.
-    opts = shlex.split(os.getenv("MPV") or "mpv")
+    opts = shlex.split(os.getenv("MPV") or "@mpv@")
     if args.focus:
-        opts.extend(["--focus-on-open"])
+        opts.extend(["--focus-on=all"])
     else:
-        opts.extend(["--no-focus-on-open"])
+        opts.extend(["--focus-on=never"])
     opts.extend(
         ["--no-terminal", "--force-window", "--input-ipc-server=" + socket_path, "--"]
     )
@@ -84,4 +132,6 @@ else:
 
     # Popen instead of check_call to start child process and exit immediately
     # stdout and stderr are sent to /dev/null so it won't print to shell env
-    subprocess.Popen(opts, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print(" ".join(opts))
+    return_code, output = capture_subprocess_output(opts)
+    exit(return_code)
