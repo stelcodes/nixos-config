3db7b8bc91dc93a362b9b4662d8699df0949c2c8
Stel Clementine <dev@stelclementine.com>
2025-04-15 16:54:39 -0700

    Update ssh keys and prune keys and secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,10 +2,6 @@ let
   keys = (import ./keys.nix);
 in
 {
-  "framework-pvpn-fast-wg-quick-config.age".publicKeys = [ keys.systemKeys.yuffie ] ++ keys.allAdminKeys;
-  "meshify-pvpn-fast-wg-quick-config.age".publicKeys = [ keys.systemKeys.terra ] ++ keys.allAdminKeys;
-  "meshify-pvpn-sc-wg-quick-config.age".publicKeys = [ keys.systemKeys.terra ] ++ keys.allAdminKeys;
-  "vpn-1.age".publicKeys = keys.allKeys;
   "admin-password.age".publicKeys = keys.allKeys;
   "root-password.age".publicKeys = keys.allKeys;
 }
