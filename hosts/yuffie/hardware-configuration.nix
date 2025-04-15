c63f4d6fdf6e0b90474a6effec301bc6791062f5
Stel Clementine <dev@stelclementine.com>
2025-04-14 18:37:53 -0700

    Update yuffie hardware

--- hosts/yuffie/hardware-configuration.nix
+++ hosts/yuffie/hardware-configuration.nix
@@ -5,13 +5,52 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    {
+      device = "/dev/disk/by-uuid/dd5cc284-0bf3-462a-9d55-540553d4ed9a";
+      fsType = "btrfs";
+      options = [ "subvol=root" ];
+    };
+
+  boot.initrd.luks.devices."crypted".device = "/dev/disk/by-uuid/18513379-5f32-4275-a9c7-21b903815212";
+
+  fileSystems."/.swapvol" =
+    {
+      device = "/dev/disk/by-uuid/dd5cc284-0bf3-462a-9d55-540553d4ed9a";
+      fsType = "btrfs";
+      options = [ "subvol=swap" ];
+    };
+
+  fileSystems."/boot" =
+    {
+      device = "/dev/disk/by-uuid/5F65-9685";
+      fsType = "vfat";
+    };
+
+  fileSystems."/home" =
+    {
+      device = "/dev/disk/by-uuid/dd5cc284-0bf3-462a-9d55-540553d4ed9a";
+      fsType = "btrfs";
+      options = [ "subvol=home" ];
+    };
+
+  fileSystems."/nix" =
+    {
+      device = "/dev/disk/by-uuid/dd5cc284-0bf3-462a-9d55-540553d4ed9a";
+      fsType = "btrfs";
+      options = [ "subvol=nix" ];
+    };
+
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
